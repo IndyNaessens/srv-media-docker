3c4e8a1714f0620ac61e7aa5e4f65b99ea61f545
Indy Naessens <indy.naessens@protonmail.com>
2019-01-28 09:26:13 +0100

    fixed update script for crontab + no depends (not needed)

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose -f /home/indy/docker/docker-compose.yml pull
-docker-compose -f /home/indy/docker/docker-compose.yml up -d --no-deps --quiet-pull
+/usr/local/bin/docker-compose -f /home/indy/docker/docker-compose.yml pull
+/usr/local/bin/docker-compose -f /home/indy/docker/docker-compose.yml up -d --quiet-pull
 
 #remove all container images that have no tag anymore (tag becomes none after new image is pulled with tag latest?)
 for image in $(docker images | grep '<none>.*' | tr -s [:blank:] | cut -d '>' -f 2 | cut -d ' ' -f 2); do docker image rm ${image}; done
