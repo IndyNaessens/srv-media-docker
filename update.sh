54b5a3bb56012bc91a825674f45fae9d1c7e412e
Indy Naessens <indy.naessens@pm.me>
2020-12-28 19:46:57 +0100

    Changes

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f $(realpath docker-compose.yml) pull 	# pull latest images
-/usr/local/bin/docker-compose -f $(realpath docker-compose.yml) up -d	# stop, recreate and start container if necessary (new image or config)
-/usr/bin/docker image prune -af						# remove images that have no container associated to them
+/usr/local/bin/docker-compose -f /home/indy/docker/compose/docker-compose.yml pull 	    # pull latest images
+/usr/local/bin/docker-compose -f /home/indy/docker/compose/docker-compose.yml up -d     # stop, recreate and start container if necessary (new image or config)
+/usr/bin/docker image prune -af						                                    # remove images that have no container associated to them
